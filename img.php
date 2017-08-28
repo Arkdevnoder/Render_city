7257f7f978fdd148bdc3fa2cbc264aad2900d5ed
Darkspive <qfybafcc@yandex.ru>
2017-08-28 12:23:25 +0500

    Создание первого коммита
    123 check

---
+++ img.php
@@ -0,0 +1,19 @@
+<?php
+
+class renderer extends commands\main {
+
+  public function __construct(){
+  
+  }
+  
+  public function get_img_multipart(){
+    
+  }
+  
+  public function __destruct(){
+    return null;
+  }
+  
+}
+
+?>
